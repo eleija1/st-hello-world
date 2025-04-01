24fce69168c7e2fa09b91018acb2f054caf515ae
eleija1 <133179691+eleija1@users.noreply.github.com>
2025-03-31 21:51:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,6 @@ from PIL import Image
 
 
 # Set the page configuration of the app, including the page title, icon, and layout.
-st.set_page_config(page_title="Timelytics", page_icon=":pencil:", layout="wide")
-
-
+st.set_page_config(page_title="Hello World", page_icon=":pencil:", layout="wide")
 
 st.write('Hello world!')
